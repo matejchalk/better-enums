01c9a85b8a46041815e91e3b36213b1491a2f51a
Matěj Chalk <matejchalk@gmail.com>
2023-07-13 21:12:30 +0200

    Implement $enum function

--- src/enum.ts
+++ src/enum.ts
@@ -1,14 +1,57 @@
+import {
+  createEntryGuards,
+  createKeyGuards,
+  createValueGuards,
+} from './guards';
 import { BasicEnum, LabeledEnum } from './types';
 
 export function $enum<T extends string | number>(values: T[]): BasicEnum<T>;
 
-export function $enum<const T extends Record<PropertyKey, string | number>>(
+export function $enum<const T extends Record<string, string | number>>(
   obj: T
 ): LabeledEnum<T>;
 
 export function $enum(
-  param: (string | number)[] | Record<PropertyKey, string | number>
+  param: (string | number)[] | Record<string, string | number>
 ) {
-  // TODO: implement
-  return {} as any;
+  if (Array.isArray(param)) {
+    return basicEnum([...param]);
+  }
+  return labeledEnum(param);
+}
+
+function basicEnum<T extends string | number>(values: T[]): BasicEnum<T> {
+  return {
+    values: () => values,
+    ...createValueGuards(values),
+  };
+}
+
+function labeledEnum<const T extends Record<string, string | number>>(
+  obj: T
+): LabeledEnum<T> {
+  if (
+    Object.values(obj).some(value => typeof value === 'string') &&
+    Object.values(obj).some(value => typeof value === 'number')
+  ) {
+    return labeledEnum(
+      Object.fromEntries(
+        Object.entries(obj).filter(([, value]) => typeof value === 'number')
+      ) as T
+    );
+  }
+
+  const objInverted = Object.fromEntries(
+    Object.entries(obj).map(([key, value]) => [value, key])
+  );
+
+  return {
+    ...basicEnum(Object.values(obj) as T[keyof T][]),
+    keys: () => Object.keys(obj),
+    ...createKeyGuards(Object.keys(obj)),
+    entries: () => Object.entries(obj) as any,
+    ...createEntryGuards(obj, objInverted),
+    obj: Object.freeze(obj),
+    keyOf: value => objInverted[value],
+  };
 }
---
+++ src/guards.ts
@@ -0,0 +1,71 @@
+export function createValueGuards<T extends string | number>(values: T[]) {
+  return createGuards('Value', values);
+}
+
+export function createKeyGuards<T extends string>(keys: T[]) {
+  return createGuards('Key', keys);
+}
+
+export function createEntryGuards<T extends Record<string, string | number>>(
+  obj: T,
+  objInverted: Record<string | number, string>
+) {
+  return createGuards<[keyof T, T[keyof T]], 'Entry'>('Entry', entry => {
+    if (!Array.isArray(entry) || entry.length !== 2) {
+      return 'Enum entry must be a tuple (e.g. ["key", "value"])';
+    }
+    if (!(entry[0] in obj)) {
+      return `Enum key out of range (received ${JSON.stringify(
+        entry[0]
+      )}, expected one of: ${Object.keys(obj)
+        .map(key => JSON.stringify(key))
+        .join(', ')})`;
+    }
+    if (obj[entry[0]] !== entry[1]) {
+      return `Enum key and value don't match (expected [${JSON.stringify(
+        entry[0]
+      )}, ${JSON.stringify(obj[entry[0]])}] or [${JSON.stringify(
+        objInverted[entry[1]]
+      )}, ${JSON.stringify(entry[1])}])`;
+    }
+    return true;
+  });
+}
+
+function createGuards<TItem, TKind extends 'Key' | 'Value' | 'Entry'>(
+  kind: TKind,
+  valuesOrPredicate: TItem[] | ((val: unknown) => true | string)
+): { [K in `is${TKind}`]: (val: unknown) => val is TItem } & {
+  [K in `assert${TKind}`]: (val: unknown) => asserts val is TItem;
+} {
+  const isFn = (value: unknown): boolean => {
+    if (typeof valuesOrPredicate === 'function') {
+      return valuesOrPredicate(value) === true;
+    }
+    return valuesOrPredicate.includes(value as TItem);
+  };
+
+  const assertFn = (value: unknown): void => {
+    if (typeof valuesOrPredicate === 'function') {
+      const res = valuesOrPredicate(value);
+      if (typeof res === 'string') {
+        throw new RangeError(res);
+      }
+    } else {
+      if (!valuesOrPredicate.includes(value as TItem)) {
+        throw new RangeError(
+          `Enum ${kind.toLowerCase()} out of range (received ${JSON.stringify(
+            value
+          )}, expected one of: ${valuesOrPredicate
+            .map(item => JSON.stringify(item))
+            .join(', ')})`
+        );
+      }
+    }
+  };
+
+  return {
+    [`is${kind}`]: isFn,
+    [`assert${kind}`]: assertFn,
+  } as any;
+}
