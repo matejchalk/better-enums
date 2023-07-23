447ebf1dd5e8f9e697499a02e7dd4050b4f4bed7
Matěj Chalk <matejchalk@gmail.com>
2023-07-23 13:41:54 +0200

    Tweak TypeDoc configuration

--- src/enum.ts
+++ src/enum.ts
@@ -3,4 +3,5 @@ import { exclude } from './exclude';
 import { extend } from './extend';
 import type { IEnum } from './types';
 
+/** @ignore */
 export const Enum: IEnum = Object.assign(create, { extend, exclude });
